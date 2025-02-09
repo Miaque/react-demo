3415a0c150179d3ab6b4a8dbb1e678223a2a12bd
miaque <miaque@163.com>
2025-02-09 20:05:16 +0800

    添加prettier支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import "./App.css";
-import Section from "./Section.tsx";
-import Heading from "./Heading.tsx";
+import './App.css'
+import Section from './Section.tsx'
+import Heading from './Heading.tsx'
 
 function Post({ title, body }: { title: string; body: string }) {
   return (
@@ -10,17 +10,17 @@ function Post({ title, body }: { title: string; body: string }) {
         <i>{body}</i>
       </p>
     </Section>
-  );
+  )
 }
 
 function RecentPosts() {
   return (
     <Section>
       <Heading>最近的帖子</Heading>
-      <Post title="里斯本的味道" body="...那些蛋挞！" />
-      <Post title="探戈节奏中的布宜诺斯艾利斯" body="我爱它！" />
+      <Post title='里斯本的味道' body='...那些蛋挞！' />
+      <Post title='探戈节奏中的布宜诺斯艾利斯' body='我爱它！' />
     </Section>
-  );
+  )
 }
 
 function AllPosts() {
@@ -29,21 +29,21 @@ function AllPosts() {
       <Heading>帖子</Heading>
       <RecentPosts />
     </Section>
-  );
+  )
 }
 
 export function ProfilePage() {
   return (
     <Section>
       <Heading>My Profile</Heading>
-      <Post title="旅行者，你好！" body="来看看我的冒险。" />
+      <Post title='旅行者，你好！' body='来看看我的冒险。' />
       <AllPosts />
     </Section>
-  );
+  )
 }
 
 function App() {
-  return <ProfilePage />;
+  return <ProfilePage />
 }
 
-export default App;
+export default App

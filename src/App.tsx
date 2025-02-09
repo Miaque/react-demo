15cdebf74eef41db6ecf2631caadbddad179ac0f
miaque <miaque@163.com>
2025-02-09 22:18:20 +0800

    排查tailwindcss样式问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,28 @@
-import './App.css'
-import Section from './Section.tsx'
-import Heading from './Heading.tsx'
-
-function Post({ title, body }: { title: string; body: string }) {
-  return (
-    <Section isFancy={true}>
-      <Heading>{title}</Heading>
-      <p>
-        <i>{body}</i>
-      </p>
-    </Section>
-  )
-}
-
-function RecentPosts() {
-  return (
-    <Section>
-      <Heading>最近的帖子</Heading>
-      <Post title='里斯本的味道' body='...那些蛋挞！' />
-      <Post title='探戈节奏中的布宜诺斯艾利斯' body='我爱它！' />
-    </Section>
-  )
-}
-
-function AllPosts() {
-  return (
-    <Section>
-      <Heading>帖子</Heading>
-      <RecentPosts />
-    </Section>
-  )
-}
-
-export function ProfilePage() {
-  return (
-    <Section>
-      <Heading>My Profile</Heading>
-      <Post title='旅行者，你好！' body='来看看我的冒险。' />
-      <AllPosts />
-    </Section>
-  )
-}
+import { Button, Input } from '@mui/material'
+import { useEffect, useRef } from 'react'
+import SendIcon from '@mui/icons-material/Send'
 
 function App() {
-  return <ProfilePage />
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleClick = () => {
+    inputRef.current?.focus()
+  }
+
+  useEffect(() => {
+    console.log('inputRef', inputRef.current)
+  }, [])
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="flex gap-2">
+        <Input ref={inputRef} />
+        <Button size='small' endIcon={<SendIcon />} onClick={handleClick}>
+          聚焦输入框
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import './index.css'
 import App from './App.tsx'
+import { CssBaseline } from '@mui/material'
+
+const theme = createTheme({
+  typography: {
+    fontFamily:
+      'HarmonyOS_Regular, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Lato, Roboto, "PingFang SC", "Microsoft YaHei", sans-serif'
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 )
